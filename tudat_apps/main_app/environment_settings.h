632efbf78704d2cb1f285a58d615f19069597f64
matt <mmm>
2020-03-13 19:47:18 +0100

    Temporarily commented all of environment settings. Also .gitignored bin/ files

--- tudat_apps/main_app/environment_settings.h
+++ tudat_apps/main_app/environment_settings.h
@@ -5,38 +5,38 @@
 #ifndef TUDATBUNDLE_ENVIRONMENT_SETTINGS_H
 #define TUDATBUNDLE_ENVIRONMENT_SETTINGS_H
 
-using namespace tudat::basic_astrodynamics;
-using namespace tudat::simulation_setup;
-using namespace tudat::propagators;
-using namespace tudat::numerical_integrators;
+//using namespace tudat::basic_astrodynamics;
+//using namespace tudat::simulation_setup;
+//using namespace tudat::propagators;
+//using namespace tudat::numerical_integrators;
 
-namespace environment_custom{
+//namespace environment_custom{
 
-    class MagneticField {
-    public:
-        123;
+//    class MagneticField {
+//    public:
+//        123;
 
-        MagneticField()
+//        MagneticField()
 
-    private:
-        456;
-    };
+//    private:
+//        456;
+//    };
 
 
 
-double getMagFieldStrength(double B0, double phi0, double r0, double r){
+//double getMagFieldStrength(double B0, double phi0, double r0, double r){
 
-    double Br_0 = B0*std::cos(phi0);
-    double Bphi_0 = B0*std::sin(phi0);
+//    double Br_0 = B0*std::cos(phi0);
+//    double Bphi_0 = B0*std::sin(phi0);
 
-    double BR = Br_0 * std::pow( (r0/r), 2);
-    double Bphi = Bphi_0 * (r0/r);
+//    double BR = Br_0 * std::pow( (r0/r), 2);
+//    double Bphi = Bphi_0 * (r0/r);
 
-    double Bmag = std::sqrt(std::pow(BR, 2) + std::pow(Bphi, 2));
+//    double Bmag = std::sqrt(std::pow(BR, 2) + std::pow(Bphi, 2));
 
-    return Bmag
-}
+//    return Bmag
+//}
 
-}
+//}
 
 #endif //TUDATBUNDLE_ENVIRONMENT_SETTINGS_H
