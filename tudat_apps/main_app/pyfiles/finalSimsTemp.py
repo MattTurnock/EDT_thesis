Added rotation coefficient and transitional tether to modelling. Fixed weirdness in current generatin somewhat. Started process of changing configuration parameter exploration values.

tudat_apps/main_app/pyfiles/finalSimsTemp.py
@@ -12,10 +12,10 @@ year = 365*24*60*60
 AU = 1.496E11
 
 doingSSO = False
-doingSOKGA = True
+doingSOKGA = False
 doingInO = False
 doingCompare = False
-doingSSOSensitivity = False
+doingSSOSensitivity = True
 
 fullLoadTodoList = ["bodyData", "propData", "thrustData", "currentVNVData"]
 
@@ -206,7 +206,7 @@ if doingSSOSensitivity:
     listOfJsons = []
     listOfDatas = []
     rejectedJsons = []
-    rejectionValue = 10
+    rejectionValue = 1
 
     SSOSensitivityMasterSubdir = "SSO-Configs-Sensitivity/"
     SSOSensitivityJsonDirPath = os.path.join(utils.jsonInputs_dir, "finalSims", "SSO_Config_Sensitivity")
@@ -218,6 +218,7 @@ if doingSSOSensitivity:
     thrustDataBase = dataSetBase[6]
     configDataBase = dataSetBase[8]
     spacecraftMassBase = configDataBase[4]
+    propDataBase = dataSetBase[5]
 
     meanThrustBase = np.mean(thrustDataBase[:, 1])
     meanAccelBase = np.mean(thrustDataBase[:, 1])/spacecraftMassBase
@@ -233,14 +234,17 @@ if doingSSOSensitivity:
 
             if thisType in thisPath:
                 thisTypeJsonList.append(allSSOSensitivityJsonFiles[j])
-                thisDataSet = utils.getAllSimDataFromJson(thisPath)
+                try:
+                    thisDataSet = utils.getAllSimDataFromJson(thisPath)
+                except FileNotFoundError:
+                    print("ERROR loading data from: %s" %thisPath)
                 thisTypeDataList.append(thisDataSet)
 
         listOfJsons.append(thisTypeJsonList)
         listOfDatas.append(thisTypeDataList)
 
 
-    print(listOfJsons)
+    # print(listOfJsons)
     bestThrustIndices = []
     bestThrustValues = []
     bestAccelIndices = []
@@ -278,7 +282,7 @@ if doingSSOSensitivity:
 
             meaniavg = np.mean(iavgData)
             maxiavg = np.nanmax(iavgData)
-            print(maxiavg)
+            # print(maxiavg)
             if abs(maxiavg) > rejectionValue:
                 rejectedJsons.append(thisJson)
             else:
@@ -315,7 +319,7 @@ if doingSSOSensitivity:
         worstAccelValues.append(worstAccel)
 
     # thrustAccelIndices = [bestThrustIndices, bestAccelIndices, worstThrustIndices, worstAccelIndices]
-    print(bestThrustIndices)
+    # print(bestThrustIndices)
     print("Rejected: ", rejectedJsons)
 
 
@@ -446,8 +450,9 @@ if doingSSOSensitivity:
 
         plt.savefig(os.path.join(configPlotFolder, "%s-accelPlot.png" %thisParameterType))
 
-
-
+    
+    #plot trajectory of base case
+    utils.plotTrajectoryData(propDataBase, plotType="x-y")
 
 
 plt.show()

tudat_apps/main_app/pyfiles/utils.py
@@ -136,22 +136,29 @@ referenceParkerDataArray[:,1] = referenceParkerBsnT
 ############################# Set constant values for simulation running ####################################################
 #######################################################################################################################
 
-importantRunsNumber = 100
-minorRunsNumber = 100
+importantRunsNumber = 2
+minorRunsNumber = 2
 
 lengths_m = np.logspace(3, 5, importantRunsNumber, endpoint=True )
 diameters_m = np.logspace(-4, -1, importantRunsNumber, endpoint=True )
-currents_mA = np.logspace(-2, 0, importantRunsNumber, endpoint=True )
+currents_mA = np.logspace(0, 3, importantRunsNumber, endpoint=True )
 areaRatios = np.linspace(0, 1, importantRunsNumber, endpoint=True )
-noLinesRange = np.linspace(1, 100, minorRunsNumber, endpoint=True )
-lengthRatios = np.linspace(0, 1, minorRunsNumber, endpoint=True)
+noLinesRange = np.linspace(1, 100, minorRunsNumber, endpoint=True, dtype=int )
+lengthRatios = np.linspace(0.1, 1, minorRunsNumber, endpoint=True)
+endmassMasses = np.logspace(0, 2, minorRunsNumber, endpoint=True)
+rotationCoefficients = np.linspace(0.5, 0.8, minorRunsNumber, endpoint=True)
+configSensitivityRunnerValues = (lengths_m, diameters_m, currents_mA, areaRatios, noLinesRange, lengthRatios,
+                                 endmassMasses, rotationCoefficients)
+
+
+
+# configSensitivityRunnerValues = (lengths_m, diameters_m, currents_mA, areaRatios, noLinesRange, lengthRatios, slackCoefficients,
+                                 # lineSeparationRatios, occultationCoefficients, endmassMasses)
+
+# These are for sensitivity analysis
 slackCoefficients =np.linspace(1, 1.1, minorRunsNumber, endpoint=True)
 lineSeparationRatios = np.logspace(-7, -5, minorRunsNumber, endpoint=True)
 occultationCoefficients = np.linspace(0.1, 1, minorRunsNumber, endpoint=True)
-endmassMasses = np.logspace(0, 2, minorRunsNumber, endpoint=True)
-
-configSensitivityRunnerValues = (lengths_m, diameters_m, currents_mA, areaRatios, noLinesRange, lengthRatios, slackCoefficients,
-                                 lineSeparationRatios, occultationCoefficients, endmassMasses)
 
 #######################################################################################################################
 ############################# Some useful functions ####################################################
@@ -185,8 +192,8 @@ posDiff = pos2-pos1
 
 V1 = posDiff/tDiff
 
-print("Initial Voyager 1 position: ", pos1)
-print("Initial Voyager 1 Velocity: ", V1)
+# print("Initial Voyager 1 position: ", pos1)
+# print("Initial Voyager 1 Velocity: ", V1)
 
 
 # Function to get true anomaly from a, e, r (NOTE: hyperbolic only, i think). Returns in degrees